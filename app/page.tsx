70f42e474710cb0e6e8ad6ad71a7df636b2b2da2
ffo87 <fabio.capua@giftty.com.br>
2026-03-20 09:01:55 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,7 @@ import Image from "next/image";
 import { useState } from "react";
 import { createClient } from "@supabase/supabase-js";
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+const supabase = null as any;
 
 const cards = [
   {
